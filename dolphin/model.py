614ef2890a4b203ef46e8e655561425c5ebafadd
yangyang <mengyangyang@speechocean.com>
2025-01-22 11:36:56 +0800

    add language and region detect func

--- dolphin/model.py
+++ dolphin/model.py
@@ -23,7 +23,11 @@ from espnet2.text.build_tokenizer import build_tokenizer
 from espnet2.text.token_id_converter import TokenIDConverter
 from espnet2.bin.s2t_inference import (Speech2Text, ListOfHypothesis, ScoreFilter)
 
-from .constants import (SPEECH_LENGTH, SAMPLE_RATE, FIRST_TIME_SYMBOL, LAST_TIME_SYMBOL, NOTIME_SYMBOL)
+from .constants import (SPEECH_LENGTH, SAMPLE_RATE, FIRST_TIME_SYMBOL, LAST_TIME_SYMBOL, NOTIME_SYMBOL,
+                        FIRST_LANG_SYMBOL, LAST_LANG_SYMBOL, FIRST_REGION_SYMBOL, LAST_REGION_SYMBOL)
+
+
+logger = logging.getLogger("dolphin")
 
 
 class DolphinSpeech2Text(Speech2Text):
@@ -52,10 +56,8 @@ class DolphinSpeech2Text(Speech2Text):
         quantize_lm: bool = False,
         quantize_modules: List[str] = ["Linear"],
         quantize_dtype: str = "qint8",
-        # default values that can be overwritten in __call__
-        lang_sym: str = "<zh>",
         task_sym: str = "<asr>",
-        predict_time: bool = False,
+        predict_time: bool = True,
     ):
 
         qconfig_spec = set([getattr(torch.nn, q) for q in quantize_modules])
@@ -66,7 +68,7 @@ class DolphinSpeech2Text(Speech2Text):
         s2t_model.to(dtype=getattr(torch, dtype)).eval()
 
         if quantize_s2t_model:
-            logging.info("Use quantized s2t model for decoding.")
+            logger.info("Use quantized s2t model for decoding.")
             s2t_model = torch.quantization.quantize_dynamic(s2t_model, qconfig_spec=qconfig_spec, dtype=quantize_dtype)
 
         decoder = s2t_model.decoder
@@ -89,7 +91,7 @@ class DolphinSpeech2Text(Speech2Text):
             lm, lm_train_args = LMTask.build_model_from_file(lm_train_config, lm_file, device)
 
             if quantize_lm:
-                logging.info("Use quantized lm for decoding.")
+                logger.info("Use quantized lm for decoding.")
                 lm = torch.quantization.quantize_dynamic(lm, qconfig_spec=qconfig_spec, dtype=quantize_dtype)
 
             scorers["lm"] = lm.lm
@@ -135,9 +137,9 @@ class DolphinSpeech2Text(Speech2Text):
             ]
             if len(non_batch) == 0:
                 beam_search.__class__ = BatchBeamSearch
-                logging.info("BatchBeamSearch implementation is selected.")
+                logger.info("BatchBeamSearch implementation is selected.")
             else:
-                logging.warning(
+                logger.warning(
                     f"As non-batch scorers {non_batch} are found, "
                     f"fall back to non-batch implementation."
                 )
@@ -146,13 +148,12 @@ class DolphinSpeech2Text(Speech2Text):
             for scorer in scorers.values():
                 if isinstance(scorer, torch.nn.Module):
                     scorer.to(device=device, dtype=getattr(torch, dtype)).eval()
-            logging.info(f"Beam_search: {beam_search}")
-            logging.info(f"Decoding device={device}, dtype={dtype}")
+            logger.info(f"Decoding device={device}, dtype={dtype}")
 
         bpemodel = os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets/bpe.model")
         tokenizer = build_tokenizer(token_type="bpe", bpemodel=bpemodel)
         converter = TokenIDConverter(token_list=token_list)
-        logging.info(f"Text tokenizer: {tokenizer}")
+        logger.info(f"Text tokenizer: {tokenizer}")
 
         self.s2t_model = s2t_model
         self.s2t_train_args = s2t_train_args
@@ -166,7 +167,6 @@ class DolphinSpeech2Text(Speech2Text):
         self.dtype = dtype
         self.nbest = nbest
 
-        self.lang_sym = lang_sym
         self.task_sym = task_sym
         self.predict_time = predict_time
 
@@ -206,6 +206,69 @@ class DolphinSpeech2Text(Speech2Text):
 
         return model, args
 
+    def detect_language(self, speech: torch.Tensor, **kwargs) -> Tuple[int, int]:
+        """
+        Detect language and region.
+
+        Args:
+            speech: waveform
+
+        Returns:
+            tuple(lang_id, region_id)
+        """
+        # Preapre speech
+        if isinstance(speech, np.ndarray):
+            speech = torch.tensor(speech)
+
+        # Only support single-channel speech
+        if speech.dim() > 1:
+            assert (speech.dim() == 2 and speech.size(1) == 1), f"speech of size {speech.size()} is not supported"
+            speech = speech.squeeze(1)  # (nsamples, 1) --> (nsamples,)
+
+        speech_length = int(SAMPLE_RATE * SPEECH_LENGTH)
+        # Pad or trim speech to the fixed length
+        if speech.size(-1) >= speech_length:
+            speech = speech[:speech_length]
+
+        # Batchify input
+        # speech: (nsamples,) -> (1, nsamples)
+        speech = speech.unsqueeze(0).to(getattr(torch, self.dtype))
+        # lengths: (1,)
+        lengths = speech.new_full([1], dtype=torch.long, fill_value=speech.size(1))
+        batch = {"speech": speech, "speech_lengths": lengths}
+
+        batch = to_device(batch, device=self.device)
+        enc, enc_olens = self.s2t_model.encode(**batch)
+        assert enc.size(0) == 1
+
+        lang_first_id = self.converter.token2id[FIRST_LANG_SYMBOL]
+        lang_last_id = self.converter.token2id[LAST_LANG_SYMBOL]
+        region_first_id = self.converter.token2id[FIRST_REGION_SYMBOL]
+        region_last_id = self.converter.token2id[LAST_REGION_SYMBOL]
+
+        decoder = self.s2t_model.decoder
+        # detect language
+        ys = torch.tensor([self.s2t_model.sos], dtype=torch.long, device=self.device).unsqueeze(0)
+        logp, _ = decoder.batch_score(ys, [None], enc)
+        mask = torch.ones(logp.size(1), dtype=torch.bool)
+        mask[lang_first_id:lang_last_id+1] = False
+        logp[0, mask] = -np.inf
+        lang_id = logp.argmax(dim=-1).tolist()[0]
+
+        # detect region
+        ys = torch.tensor([self.s2t_model.sos, lang_id], dtype=torch.long, device=self.device).unsqueeze(0)
+        logp, _ = decoder.batch_score(ys, [None], enc)
+        mask = torch.ones(logp.size(1), dtype=torch.bool)
+        mask[region_first_id:region_last_id+1] = False
+        logp[0, mask] = -np.inf
+        region_id = logp.argmax(dim=-1).tolist()[0]
+
+        lang_symbol = self.converter.ids2tokens([lang_id])[0]
+        region_symbol = self.converter.ids2tokens([region_id])[0]
+
+        logger.info(f"detect language: {lang_symbol}, region: {region_symbol}")
+        return lang_id, region_id
+
     @torch.no_grad()
     @typechecked
     def __call__(
@@ -233,11 +296,14 @@ class DolphinSpeech2Text(Speech2Text):
 
         """
 
-        lang_sym = lang_sym if lang_sym is not None else self.lang_sym
         predict_time = predict_time if predict_time is not None else self.predict_time
 
-        lang_id = self.converter.token2id[lang_sym]
-        region_id = self.converter.token2id[region_sym]
+        if all([lang_sym, region_sym]):
+            lang_id = self.converter.token2id[lang_sym]
+            region_id = self.converter.token2id[region_sym]
+        else:
+            lang_id, region_id = self.detect_language(speech)
+
         task_id = self.converter.token2id["<asr>"]
         notime_id = self.converter.token2id[NOTIME_SYMBOL]
         first_time_id = self.converter.token2id[FIRST_TIME_SYMBOL]
@@ -273,7 +339,7 @@ class DolphinSpeech2Text(Speech2Text):
         # lengths: (1,)
         lengths = speech.new_full([1], dtype=torch.long, fill_value=speech.size(1))
         batch = {"speech": speech, "speech_lengths": lengths}
-        logging.info("speech length: " + str(speech.size(1)))
+        logger.info("speech length: " + str(speech.size(1)))
 
         # a. To device
         batch = to_device(batch, device=self.device)
